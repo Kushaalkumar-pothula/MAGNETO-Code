9a9f7488e6f869a9d3ac153b8ce6f9cac4409c8a
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-02-20 23:20:03 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+echo "Starting MAGNETO"
+
+echo "Initialising simulation"
 python FlareTimes.py
+echo "Simulation complete"
+
+echo "Initialising analysis"
 python FlareTimePlot.py
+echo "Analysis complete"
