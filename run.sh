658eca6891036bb1f4a05945e4bee030896d13a6
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-02-20 23:25:39 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ echo "Starting MAGNETO at:"
 current_date_time="`date "+%Y-%m-%d %H:%M"`";
 echo $current_date_time;
 
-echo "Initialising simulation"
+echo "Initializing simulation"
 python FlareTimes.py
 echo "Simulation complete"
 
-echo "Initialising analysis"
+echo "Initializing analysis"
 python FlareTimePlot.py
 echo "Analysis complete"
