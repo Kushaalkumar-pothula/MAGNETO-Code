e70616b31780e0e91cd18ffa5100eb1c6b089fc5
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-02-20 15:48:47 +0530

    Update FlareTimePlot.py

--- FlareTimePlot.py
+++ FlareTimePlot.py
@@ -13,10 +13,12 @@ fig.set_size_inches(8,6)
 plt.scatter(age, flare_intervals, alpha = 0.8, s = 70, c = flare_intervals, cmap = 'plasma')    #Plot flare intrvals against magnetar age
 plt.colorbar()
 
-plt.plot(age,flare_intervals, color ='black', linestyle = ':')    #Line-lot flare intrvals against magnetar age
+plt.plot(age,flare_intervals, color ='black', linestyle = ':')    #Line-plot flare intrvals against magnetar age
 
 plt.xlabel("Magnetar Age")
 plt.ylabel("Flare Interval")
 plt.grid(True)
+
+plt.savefig("Flare_Times_Result.png")
 
 #----------------------------------------------------------
