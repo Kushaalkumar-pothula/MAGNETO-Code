52074f855f9e7ec31e84e28f9fd85b33c7c5afbb
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-03-12 01:50:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@
 current_date_time="`date "+%Y-%m-%d %H:%M"`";
 echo "Starting MAGNETO at $current_date_time"
 
-
-echo "Starting MAGNETO..."
 start_time="$(date -u +%s)"
 
 echo "Starting flaring interval simulation..."
@@ -14,7 +12,6 @@ python FlareTimePlot.py &
 echo "Starting energy evolution simulation..."
 python energyevol.py &
 wait
-echo "Done"
 end_time="$(date -u +%s)"
 elapsed="$(($end_time-$start_time))"
 echo "MAGNETO simulation completed successfully in $elapsed seconds."
