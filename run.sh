aaad770f7203accd42e9b70926db113f64c76551
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-03-12 01:36:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+echo "___  ___  ___  _____  _   _  _____ _____ _____ "
+echo "|  \/  | / _ \|  __ \| \ | ||  ___|_   _|  _  |"
+echo "| .  . |/ /_\ \ |  \/|  \| || |__   | | | | | |"
+echo "| |\/| ||  _  | | __ | . ` ||  __|  | | | | | |"
+echo "| |  | || | | | |_\ \| |\  || |___  | | \ \_/ /"
+echo "\_|  |_/\_| |_/\____/\_| \_/\____/  \_/  \___/ "
+                                               
 current_date_time="`date "+%Y-%m-%d %H:%M"`";
 echo "Starting MAGNETO at $current_date_time"
 
 
-echo "Initializing simulation..."
+echo "Starting MAGNETO..."
 start_time="$(date -u +%s)"
 
+echo "Starting flaring interval simulation..."
 python FlareTimes.py
-echo "Simulation complete"
-
-echo "Initializing analysis..."
-python FlareTimePlot.py
-
-end_time="$(date -u +%s)"
-echo "Analysis complete"
-
+python FlareTimePlot.py &
+echo "Starting energy evolution simulation..."
+python energyevol.py &
+wait
+echo "Done"
 
 elapsed="$(($end_time-$start_time))"
 echo "MAGNETO simulation completed successfully in $elapsed seconds."
