2eea48f4329b831ba84e5d75ba4f5ed2d8f5053a
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-03-27 23:20:41 +0530

    Update interval_evol_modular.py

--- interval_evol_modular.py
+++ interval_evol_modular.py
@@ -6,19 +6,13 @@ from interval_module import interval_gen
 lst_a = [] 
 lst_f = []
 interval_gen(lst_a, lst_f)
-#--------------------Plot simulated data-------------------
 
 fig = plt.gcf()
 fig.set_size_inches(8, 6)
-
 # Plot flare intrvals against magnetar age
-plt.scatter(lst_a, lst_f, alpha=0.8, s=70, c=lst_f, cmap='plasma')
-plt.colorbar()
-# Line-lot flare intrvals against magnetar age
-#plt.plot(lst_a, lst_f, color='black', linestyle=':')
+plt.scatter(lst_a, lst_f, alpha=0.8, s=70, c='r')
 plt.xlabel("Magnetar Age [seconds]")
 plt.ylabel("Flare Interval [seconds]")
 plt.grid(True)
-plt.show()
-
-#----------------------------------------------------------
+#Save plot
+plt.savefig(f'{dirname}/flare_interval_evolution.png')
