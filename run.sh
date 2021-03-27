b242b1e2c02c2f982e5c73368de567f27916a1cf
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-03-27 23:34:28 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,10 @@ echo "Starting MAGNETO at $current_date_time"
 start_time="$(date -u +%s)"
 
 echo "Starting simulation..."
+python interval_module.py &
 python interval_evol_modular.py &
 python energy_evol_modular.py 1e46 1.3 &
 wait
 end_time="$(date -u +%s)"
 elapsed="$(($end_time-$start_time))"
-echo "MAGNETO simulation completed successfully in $elapsed seconds."
+echo "MAGNETO run completed successfully in $elapsed seconds."
